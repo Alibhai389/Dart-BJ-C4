d8f731e70e7d7b3cf3d8b41b169336e84e0e985e
Alibhai389 <https://github.com/Alibhai389/Dart-BJ-C4>
2025-07-18 23:58:49 +0500

    first commit

---
+++ main.dart
@@ -0,0 +1,173 @@
+
+import 'dart:io';
+void main(){
+print("Enter your name");
+String? name = stdin.readLineSync();
+print("Hello, $name!");
+}
+
+  
+//question no 1
+/*int length = 4;
+int breadth = 2;
+
+
+if (length==breadth) {
+  print("This is a Square");
+  
+}
+else {
+  print("This is a Rectangle");
+}*/
+
+//question no 2
+
+/*int yusuf = 32;
+int shahzeb = 25;
+if (yusuf>shahzeb) {
+  print("yusuf is older");
+  
+}
+if (shahzeb<yusuf) {
+print("shahzeb is younger") ; 
+}*/
+
+//question no 3
+
+/*int classesHeld = 16;
+int classesAttented = 10;
+double percent = (classesAttented/classesHeld)*100;
+if (percent>75) {
+  print("Sit in Exam");
+}
+else{
+  print("Get out of the Class");
+}*/
+
+//question no 5 
+
+/*int temperature = 42;
+print("Temperature: $temperature");
+if (temperature < 0) {
+print("Freezing weather");  
+}
+else if (temperature > 0 && temperature <10){
+  print("Very Cold Weather");
+}
+else if (temperature > 10 && temperature < 20){
+  print("Cold Weather");
+}
+else if (temperature > 20 && temperature < 30){
+  print("Normal Temperature");
+}
+else if (temperature > 30 && temperature < 40){
+  print("Hot Weather");
+}
+else {
+  print("Its Very Hot");
+}?*/
+
+
+//question no 6
+
+/*int sub1 = 73;
+int sub2 = 68;
+int sub3 = 89;
+int sub4 = 75;
+int sub5 = 93;
+int total = sub1+sub2+sub3+sub4+sub5;
+num percent = (total/500)*100;
+ if (percent >=90) {
+  print("A Grade");
+}
+ if (percent >=80 && percent < 90) {
+  print("B Grade");
+}
+ if (percent >=70 && percent < 80 ) {
+  print("C Grade");
+}
+ if (percent >=60 && percent < 70) {
+  print(" Grade");
+}
+ if (percent < 60) {
+  print("You Are Fail");
+}*/
+
+//question no 7
+
+/* num1 = 49;
+
+if( num1 % 2 == 0 ){
+  print("This is Even Number");
+}
+else{
+  print("This is Odd Number");
+}*/
+
+//question no 8
+
+/*int number = 15;
+if (number % 3 == 0 && number % 5 ==0) {
+print("$number is the multiple of both 3 and 5");  
+}
+else if (number % 3 == 0){
+  print("$number is multiple of 3");
+}
+else if (number % 5 == 0){
+  print("$number is multiple of 5");
+}
+else {
+  print("$number is not a multiple of both 3 and 5");
+}*/
+
+//question no 4
+/*int year = 2024;
+if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){
+  print("$year is a leap year");
+}
+else{
+  print("$year is not a leap year");
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
