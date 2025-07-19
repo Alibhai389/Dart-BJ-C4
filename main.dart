d3d44ea0d761d27a21764c6cb3ae463d7efa7102
Alibhai389 <https://github.com/Alibhai389/Dart-BJ-C4>
2025-07-19 16:28:02 +0500

    first commit

--- main.dart
+++ main.dart
@@ -1,9 +1,33 @@
 void main(){
-  List stdNames = ["ali","shahzeb","moosa"];
-print(stdNames);
-print(stdNames[0]);
-print(stdNames[1]);
-print(stdNames[2]);
+  // List stdNames = ["ali","shahzeb","moosa"];
+// print(stdNames);
+// print(stdNames[0]);
+// print(stdNames[1]);
+// print(stdNames[2]);
+// List stdMarks = [
+// [33,45,67,66,56];
+// [34,56,67,87,76,];
+// [45,54,46,65];
+// print
+
+List stdNames = ["ali","shahzeb","moosa","kinza","hamza","hello","lello","ilo"];
+print(stdNames.length);
+print(stdNames[stdNames.length-1]);
+print stdNames.clear();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
